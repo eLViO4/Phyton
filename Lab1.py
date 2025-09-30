3476f404bae868a549be5444334d632c38a7a3ac
eLViO4 <valeriyliubimyi@gmail.com>
2025-09-30 16:49:31 +0300

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,7 @@
+celsius = float(input("Celcius temperature: "))
+
+fahrenheit = 32 + (9 / 5) * celsius
+kelvin = celsius + 273.15
+
+print(f"Fahrenheit temperature: {fahrenheit:.2f} °F")
+print(f"Kelvin temperature: {kelvin:.2f} K")
