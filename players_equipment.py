f0996af97d44a39b5526165e41b65ac905fec0e9
eLViO4 <valeriyliubimyi@gmail.com>
2025-10-03 20:20:41 +0300

    Update and rename rivers.py to players_equipment.py

---
+++ players_equipment.py
@@ -0,0 +1,11 @@
+subjects = {
+    "science": {
+        "physics": ["nuclear physics", "optics", "thermodynamics"],
+        "computer science": {},
+        "biology": {}
+    },
+    "humanities": {},
+    "public": {}
+}
+print("Ключі subjects['science']:", list(subjects["science"].keys()))
+print("Значення subjects['science']['physics']:", subjects["science"]["physics"])
--- rivers.py
+++
@@ -1,7 +0,0 @@
-rivers = {
-    "Amazon": "South America",
-    "Nile": "Africa",
-    "Dnipro": "Europe"
-}
-for river, region in rivers.items():
-    print(f"The {river} runs through {region}.")
